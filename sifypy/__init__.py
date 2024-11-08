4b314993f590b42d1c384e2ae9c591c5b4d4b04e
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2024-11-08 17:16:38 -0500

    Create __init__.py

---
+++ sifypy/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sifypy is a lightweight Python library with a flexible, easy-to-use API for diverse programming tasks. It 
+enables quick, reliable operations, helping streamline workflows and enhance productivity. Perfect 
+for everything from data manipulation to automation.
+"""
+
+from ._typing.typing import *
